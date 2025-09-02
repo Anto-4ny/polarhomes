cb82836f336c130ae5e23de6a8780b33bb7e4066
Antony <toni4pius@gmail.com>
2025-09-02 16:44:57 +0300

    jquery issue solivng

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+import $ from "jquery";
 import WOW from 'wowjs';
 import 'owl.carousel';
 import 'tempusdominus-bootstrap-4';
 import 'eonasdan-bootstrap-datetimepicker';
+
+(function ($) {
+   "use strict";
+   // your same code here
+})($);
